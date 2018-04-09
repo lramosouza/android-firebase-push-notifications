85f24a9c911f39c5b49d131486b058104a1769d8
leandro.souza-at-766073738479 <lramosouza@outlook.com>
2018-04-09 10:07:04 -0300

    Innitial Commit

---
+++ app/src/main/java/br/com/futema/firebasecrud/MeuFirebaseInstanceIDService.java
@@ -0,0 +1,24 @@
+package br.com.leandro.firebasecrud;
+
+/**
+ * Created by logonrm on 07/02/2018.
+ */
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MeuFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        // Obter o novo InstanceID
+        String firebaseToken = FirebaseInstanceId.getInstance().getToken();
+        //String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String uid = FirebaseInstanceId.getInstance().getToken();
+
+        FirebaseDatabase.getInstance().getReference().child("utilizadores/"+uid+"/FirebaseToken")
+                .setValue(firebaseToken);
+
+    }
+}
